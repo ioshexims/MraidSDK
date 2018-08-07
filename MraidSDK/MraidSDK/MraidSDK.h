654a8d12aa69d966a09506feb6195f63cbfb6cd5
dev <dev@MacBook-Pro.local>
2018-08-07 16:24:45 +0530

    Initial commit

---
+++ MraidSDK/MraidSDK/MraidSDK.h
@@ -0,0 +1,19 @@
+//
+//  MraidSDK.h
+//  MraidSDK
+//
+//  Created by dev on 07/08/18.
+//  Copyright © 2018 hexims. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MraidSDK.
+FOUNDATION_EXPORT double MraidSDKVersionNumber;
+
+//! Project version string for MraidSDK.
+FOUNDATION_EXPORT const unsigned char MraidSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MraidSDK/PublicHeader.h>
+
+
